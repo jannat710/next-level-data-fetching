b2bab7fc2058cf9eb78461a76e43e644618bae49
e <jannatul0040@gmail.com>
2024-01-15 16:40:16 +0600

    displayed posts data

---
+++ src/app/posts/page.js
@@ -0,0 +1,29 @@
+
+
+const PostsPage = async() => {
+
+    const res = await fetch("http://localhost:5000/posts",{
+        cache:"force-cache",
+    });
+    const posts = await res.json();
+    // console.log(posts)
+    return (
+        <div className="w-full">
+            <h1 className="text-2xl">Total Post:{posts.length}</h1>
+            {
+                posts.map(post=><div key={post.id} className="card bg-gray-100 my-5 shadow-xl w-[70%] mx-auto">
+                <div className="card-body">
+                  <h2 className="card-title">{post.title}</h2>
+                  <p>{post.description}</p>
+                  <p>Likes: {post.likes_count}</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">See More</button>
+                  </div>
+                </div>
+              </div>)
+            }
+        </div>
+    );
+};
+
+export default PostsPage;
